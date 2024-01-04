a3b1882864a80e9ed3bcb6d2d3c8d45b21f0285b
kdjdj106 <kdjdj106@naver.com>
2024-01-04 18:25:14 +0900

    소셜로그인 및 자동 회원가입 구현

--- src/main/java/com/example/makeboard0629/service/MemberService.java
+++ src/main/java/com/example/makeboard0629/service/MemberService.java
@@ -4,6 +4,7 @@ import com.example.makeboard0629.dto.SignInDto;
 import com.example.makeboard0629.dto.SignUpDto;
 import com.example.makeboard0629.entity.Member;
 import com.example.makeboard0629.repository.MemberRepository;
+import com.example.makeboard0629.type.Authority;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -20,7 +23,7 @@ public class MemberService implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return this.memberRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("couldn't find user" + email));
+        return memberRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("couldn't find user" + email));
 
     }
 
@@ -29,15 +32,56 @@ public class MemberService implements UserDetailsService {
         if (exists){
             throw new RuntimeException("이미 사용중인 아이디 입니다");
         }
-        signUpDto.setPassword(this.passwordEncoder.encode(signUpDto.getPassword()));
-        return this.memberRepository.save(signUpDto.toMemberEntity());
+        Member member = Member.builder()
+                .userRole(Authority.ROLE_USER)
+                .email(signUpDto.getEmail())
+                .password(passwordEncoder.encode(signUpDto.getPassword()))
+                .build();
+        return memberRepository.save(member);
     }
     public Member authenticate(SignInDto signInDto){
         var member = this.memberRepository.findByEmail(signInDto.getEmail())
                 .orElseThrow(()-> new RuntimeException("존재하지 않는 ID 입니다"));
-        if (!this.passwordEncoder.matches(signInDto.getPassword(), member.getPassword())){
+        if (!passwordEncoder.matches(signInDto.getPassword(), member.getPassword())){
             throw new RuntimeException("비밀번호가 일치하지 않습니다.");
         }
         return member;
+    }
+
+    public Member oauth2Register(SignUpDto signUpDto) {
+        // 이미 가입된 아이디인지 확인
+        boolean exists = memberRepository.existsByEmail(signUpDto.getEmail());
+        if (exists){
+            System.out.println("이미 가입된 아이디 입니다. 자동 로그인을 진행합니다.");
+            // 이미 가입된 경우 아이디를 이메일로 검색
+            Optional<Member> optionalMember = memberRepository.findByEmail(signUpDto.getEmail());
+            // 못찾을 경우 NullPointerException 출력
+            Member member = optionalMember.orElseThrow(()-> new NullPointerException("해당 이메일에 맞는 유저를 찾을수 없습니다."));
+            // 비밀 번호 대조
+            if (!passwordEncoder.matches(signUpDto.getPassword(), member.getPassword())){
+                throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+            }
+
+            return member;
+
+
+        }else {
+            // 새로 가입하는 유저일경우
+            System.out.println("처음가입하는 아이디 입니다. 자동로그인을 진행합니다.");
+            // 유저 db에 저장
+            Member member = Member.builder()
+                    .email(signUpDto.getEmail())
+                    .password(passwordEncoder.encode(signUpDto.getPassword()))
+                    .userRole(Authority.ROLE_USER)
+                    .build();
+            memberRepository.saveAndFlush(member);
+
+            Optional<Member> optionalMember = memberRepository.findByEmail(signUpDto.getEmail());
+            // 못찾을 경우 NullPointerException 출력
+
+            return optionalMember.orElseThrow(()-> new NullPointerException("해당 이메일에 맞는 유저를 찾을수 없습니다."));
+
+        }
+
     }
 }

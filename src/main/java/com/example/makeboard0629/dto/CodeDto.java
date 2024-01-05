084cac3592950ac6a64d83aa3f1762f1240cc126
kdjdj106 <kdjdj106@naver.com>
2024-01-06 00:53:58 +0900

    CodeDto @NoArgsConstructor, @AllArgsConstructor 추가

--- src/main/java/com/example/makeboard0629/dto/CodeDto.java
+++ src/main/java/com/example/makeboard0629/dto/CodeDto.java
@@ -1,10 +1,14 @@
 package com.example.makeboard0629.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CodeDto {
     private String code;
 }

a9da1517d2a82a3f3979102c4070be92ed9c383e
kdjdj106 <kdjdj106@naver.com>
2024-01-08 18:45:13 +0900

    BoardRepository
    jpa수정

--- src/main/java/com/example/makeboard0629/repository/LikeRepository.java
+++ src/main/java/com/example/makeboard0629/repository/LikeRepository.java
@@ -1,17 +1,17 @@
 package com.example.makeboard0629.repository;
 
-import com.example.board.entity.Like;
-import com.example.board.entity.User;
+import com.example.makeboard0629.entity.Like;
+import com.example.makeboard0629.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    void deleteByUserUniqueIdAndBoardId(String uniqueId, Long boardId);
-    Boolean existsByUserNickNameAndBoardId(String userNickname, Long boardId);
-    List<Like> findAllByUserUniqueId(String uniqueId);
+    void deleteByUserEmailAndBoardId(String uniqueId, Long boardId);
+    Boolean existsByUserEmailAndBoardId(String userNickname, Long boardId);
+    List<Like> findAllByUserEmail(String uniqueId);
     List<Like> findAllByBoardId(Long boardId);
-    Optional<Like> findByUserAndBoardId(User user, Long board_id);
+    Optional<Like> findByUserAndBoardId(Users users, Long board_id);
 
 }

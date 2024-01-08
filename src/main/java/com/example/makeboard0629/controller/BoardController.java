01504d926497c4f7e2b76ee1b8a10ce69b6d9d3a
kdjdj106 <kdjdj106@naver.com>
2024-01-08 18:44:15 +0900

    BoardController
    uniqueId 인자 삭제 및  @AuthenticationPrincipal 추가

--- src/main/java/com/example/makeboard0629/controller/BoardController.java
+++ src/main/java/com/example/makeboard0629/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.example.makeboard0629.controller;
 
 import com.example.makeboard0629.dto.board.BoardDto;
 import com.example.makeboard0629.dto.board.BoardsDto;
+import com.example.makeboard0629.entity.Users;
 import com.example.makeboard0629.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,15 +23,16 @@ public class BoardController {
 //    private final LikeService likeService;
 
     @PostMapping("/save")
-    public ResponseEntity<?> saveBoard(Authentication authentication, @RequestBody BoardDto boardDto) {
-
-        boardService.saveBoard(boardDto, uniqueId);
+    public ResponseEntity<?> saveBoard(@AuthenticationPrincipal Users users, @RequestBody BoardDto boardDto) {
+        String logInUserEmail = users.getEmail();
+        boardService.saveBoard(boardDto, logInUserEmail);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @GetMapping("/getMyBoardList")
-    public ResponseEntity<List<BoardsDto>> getBoardList(Authentication authentication) {
-        List<BoardsDto> boardsDtoList = boardService.getMyBoards(uniqueId);
+    public ResponseEntity<List<BoardsDto>> getBoardList(@AuthenticationPrincipal Users users) {
+        String logInUserEmail = users.getEmail();
+        List<BoardsDto> boardsDtoList = boardService.getMyBoards(logInUserEmail);
         return ResponseEntity.ok(boardsDtoList);
     }
     @GetMapping("/getAllBoards")

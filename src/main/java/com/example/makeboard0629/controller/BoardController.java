5f6106ef0c0a9026c39a3c8c2672e2bb65f0cfe0
kdjdj106 <kdjdj106@naver.com>
2024-01-09 23:35:44 +0900

    BoardController
    url 수정

--- src/main/java/com/example/makeboard0629/controller/BoardController.java
+++ src/main/java/com/example/makeboard0629/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.makeboard0629.controller;
 
 import com.example.makeboard0629.dto.board.BoardDto;
+import com.example.makeboard0629.dto.board.BoardUpdateDto;
 import com.example.makeboard0629.dto.board.BoardsDto;
 import com.example.makeboard0629.entity.User;
 import com.example.makeboard0629.service.board.BoardService;
@@ -14,22 +15,31 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
 
-    @PostMapping("/saveBoard")
+    @PostMapping("/save")
     public ResponseEntity<?> saveBoard(@AuthenticationPrincipal User user, @RequestBody BoardDto boardDto) {
-
         boardService.saveBoard(boardDto, user.getEmail());
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @GetMapping("/getMyBoardList")
+    @PostMapping("/update")
+    public ResponseEntity<?> updateBoard(@AuthenticationPrincipal User user, @RequestBody BoardUpdateDto BoardUpdateDto) {
+
+        boardService.updateBoard(BoardUpdateDto, user.getEmail());
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/myBoard")
     public ResponseEntity<List<BoardsDto>> getBoardList(@AuthenticationPrincipal User user) {
         List<BoardsDto> boardsDtoList = boardService.getMyBoards(user.getEmail());
         return ResponseEntity.ok(boardsDtoList);
     }
-    @GetMapping("/getAllBoards")
+    @GetMapping("/AllBoards")
     public ResponseEntity<List<BoardsDto>> getAllBoards(){
         List<BoardsDto> boardsDtoList = boardService.getAllBoards();
         return ResponseEntity.ok(boardsDtoList);

7e198e6bd21546d8db4fb8de63f0b072c676a59e
kdjdj106 <kdjdj106@naver.com>
2024-01-09 18:25:15 +0900

    오류나는 코드 삭제

--- src/main/java/com/example/makeboard0629/service/JwtService.java
+++ src/main/java/com/example/makeboard0629/service/JwtService.java
@@ -24,13 +24,11 @@ public class JwtService {
     @Transactional
     public void login(TokenDto token, SignInDto signInDto){
         String loginUserEmail = signInDto.getEmail();
-//        Optional<RefreshToken> optionalRefreshToken = Optional.ofNullable(refreshTokenRepository
-//                .findByKeyEmail(loginUserEmail).orElseThrow(() -> new NullPointerException("해당 이메일에 맞는 리스페시 토큰을 찾을수 없습니다.")));
-//        RefreshToken refreshToken = optionalRefreshToken.get();
-        if(refreshTokenRepository.existsByKeyEmail(loginUserEmail)){
+        if (refreshTokenRepository.findByKeyEmail(loginUserEmail).isPresent()){
             log.info("기존의 존재하는 refresh 토큰 삭제");
             refreshTokenRepository.deleteByKeyEmail(loginUserEmail);
         }
+
         RefreshToken refreshToken = RefreshToken.builder()
                 .keyEmail(loginUserEmail)
                 .refreshToken(token.getRefreshToken())

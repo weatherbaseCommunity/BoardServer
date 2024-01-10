ccf05415e985bf26732928374b8ffa501ffdd2fd
kdjdj106 <kdjdj106@naver.com>
2024-01-10 18:14:06 +0900

    수정

--- src/main/java/com/example/makeboard0629/controller/TestController.java
+++ src/main/java/com/example/makeboard0629/controller/TestController.java
@@ -1,10 +1,17 @@
 package com.example.makeboard0629.controller;
 
+import com.example.makeboard0629.entity.Board;
+import com.example.makeboard0629.entity.Comment;
+import com.example.makeboard0629.entity.Like;
+import com.example.makeboard0629.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -12,8 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 //@RequestMapping("/test")
 public class TestController {
-
-
     @GetMapping("/user")
     @PreAuthorize("hasAnyRole('USER')")
     public ResponseEntity<?> testuser() {
